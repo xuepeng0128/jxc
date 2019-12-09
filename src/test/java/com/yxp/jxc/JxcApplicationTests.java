7b883c794ff9a4c963eb50cfc7b847c70b60a0be
yxpmatrix <2426574593@qq.com>
2019-12-09 10:18:23 +0800

    new project

---
+++ src/test/java/com/yxp/jxc/JxcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxp.jxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
